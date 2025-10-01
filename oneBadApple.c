3d39fc26f8b0a58489eef1e6830545c832fe5d9d
Connor Valley <valleyco@mail.gvsu.edu>
2025-10-01 15:06:41 -0400

    semi-working version

--- oneBadApple.c
+++ oneBadApple.c
@@ -4,21 +4,28 @@
  * CIS 451 Project 1 (F25)
  *
  * !!! Name(s) !!!
+ * Charlie Greco
+ * Connor Valley
  *************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
+struct apple {
+    int recipient;
+    char message[250];
+};
 
 int main(int argc, char *argv[]) {
     int message_size = 300;
     int k;
-    printf("Enter how many processes I should be in the ring? ");
+    printf("Enter how many processes should be in the ring? ");
     if (scanf("%d", &k) != 1){
         printf("Invalid Input.\n");
         exit(1);
     }
+    getchar();
     printf("Creating a ring with %d processes\n", k);
     
     //Create an array that holds all the pipes
@@ -33,7 +40,12 @@ int main(int argc, char *argv[]) {
 
     int node_index; //Keep track of the nodes index for the pipes array
     pid_t pid;
+    
+    struct apple apl;
     char message_input[message_size];
+    int recipient;
+
+
     for (node_index=1;node_index<k;node_index++){
         if( (pid = fork()) < 0){
             perror("Fork Failure");
@@ -46,32 +58,75 @@ int main(int argc, char *argv[]) {
 
     node_index = node_index % k;
     printf("This is node %d\n", node_index);
-    int next_node = (node_index +1) %k;
+    int next_node = (node_index +1) % k;
 
-    //Create a wait for all the processes to finish creating (not sure if this is needed)
-
-
-    char temp_string[] = "Hellow there"; // Temporary string (replace with user input later)
-    
-    if(pid != 0){ // Also temporary (move inside of loop)
-        close(pipes[next_node][0]);
-        write(pipes[next_node][1], &temp_string, sizeof(temp_string));   
+    for (int i = 0; i < k; i++) {
+        if (i != node_index) {
+            close(pipes[i][0]); // close all read ends except myself
+        }
+        if (i != next_node) {
+            close(pipes[i][1]); // close all write ends besides the next node
+        }
     }
-    while (1){
-        
-        // If statement for the parent function to promt the user
-        
-        //Use read to wait for a message on pipes[node_index][0]
-        close(pipes[node_index][1]); //Close the writing pipe
-        read(pipes[node_index][0], &message_input, sizeof(message_input));
-        printf("Received by node %d, message is: %s\n", node_index,message_input);
-        
-        // Add code to determine if the message is intended for this node
+    
+    if (pid != 0) {
+        sleep(1);   // parent waits for process creation
 
-        //Write the message to the next pipe
-        printf("Sending message to node %d\n", next_node);
-        close(pipes[next_node][0]);
-        write(pipes[next_node][1], &message_input, sizeof(message_input));
+        // initial prompt for user input to start loop
+
+        printf("Enter Message: ");
+        fgets(message_input, sizeof(message_input), stdin);
+        message_input[strcspn(message_input, "\n")] = '\0';
+
+        printf("Enter Destination Node (0-%d): ", k-1);
+        scanf("%d", &recipient);
+        getchar();
+
+        apl.recipient = recipient;
+        strcpy(apl.message, message_input);
+
+        write(pipes[next_node][1], &apl, sizeof(apl));
+    }
+
+
+    // main loop
+
+    while(1) {
+
+        read(pipes[node_index][0], &apl, sizeof(apl));
+        
+        if (strlen(apl.message) > 0) {
+            if (apl.recipient == node_index) {
+                printf("Node %d received message: %s\n", node_index, apl.message);
+
+                apl.message[0] = '\0';
+            } else {
+                printf("Node %d forwarding message...\n", node_index);
+            }
+
+        } else {
+
+            printf("Node %d sees empty message...\n", node_index);
+
+            if (node_index == 0) {
+
+                printf("Enter Message: ");
+                fgets(message_input, sizeof(message_input), stdin);
+                message_input[strcspn(message_input, "\n")] = '\0';
+
+                printf("Enter Destination Node (0-%d): ", k-1);
+                scanf("%d", &recipient);
+                getchar();
+
+                apl.recipient = recipient;
+                strcpy(apl.message, message_input);
+            }
+        }
+
+
+
+        write(pipes[next_node][1], &apl, sizeof(apl));
+
     }
 
     return 0;
